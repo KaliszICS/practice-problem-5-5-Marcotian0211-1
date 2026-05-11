51f3e042e5095c29e150614457cbd8412ac963ff
Marcotian0211 <350327743@tdsb.ca>
2026-05-11 03:35:34 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,15 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+public static boolean isPalindrome(String word) {
+    String word2 = word.replace(" ", "");
+	String word3=word2.toLowerCase();
+    for (int i = 0; i < word3.length() / 2; i++) {
+        if (word3.charAt(i) != word3.charAt(word3.length() - 1 - i)) {
+            return false;
+        }
+    }
 
+    return true;
+}
 }
